3a62c46c0486f89cbf2cfecb3ff99a84169265fd
anders.poirel <contact@anderspoirel.net>
2025-06-23 02:17:41 -0700

    add tests for simple file cases

--- tests/test_files.py
+++ tests/test_files.py
@@ -1,17 +1,20 @@
 import io
+from databricks.sdk.service.files import DownloadResponse
+
+from localbricks.files import open_file
 
 
 class FakeWorkspaceClient:
-    def __init__(self):
-        self.files = FakeFilesAPI()
+    def __init__(self, file_contents: dict[str, bytes]):
+        self.files = FakeFilesAPI(file_contents)
 
 
 class FakeFilesAPI:
-    def __init__(self):
-        self.files: dict[str, bytes] = {}
+    def __init__(self, contents: dict[str, bytes]):
+        self.contents = contents
 
-    def download(self, file_path: str) -> io.BytesIO:
-        return io.BytesIO(self.files[file_path])
+    def download(self, file_path: str) -> DownloadResponse:
+        return DownloadResponse(contents=io.BytesIO(self.contents[file_path]))
 
     def upload(
         self,
@@ -19,4 +22,20 @@ class FakeFilesAPI:
         contents: io.BytesIO,
         overwrite: bool = True,
     ) -> None:
-        self.files[file_path] = contents.getvalue()
+        self.contents[file_path] = contents.getvalue()
+
+
+def test_open_file_read_text_mode():
+    file_path = "/Volumes/my_catalog/my_schema/my_volume/test.txt"
+    client = FakeWorkspaceClient({file_path: b"Hi, Mr. Fox!"})
+    with open_file(file_path, "r", client=client) as f:
+        assert f.read() == "Hi, Mr. Fox!"
+
+
+def test_open_file_write_text_mode():
+    file_path = "/Volumes/my_catalog/my_schema/my_volume/test.txt"
+    client = FakeWorkspaceClient({})
+    with open_file(file_path, "w", client=client) as f:
+        f.write("Hi, Mr. Fox!")
+    contents = client.files.contents[file_path]
+    assert contents == b"Hi, Mr. Fox!"
