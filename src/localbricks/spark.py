7cdab7ec2ad984383ceddf55d7304334237ba7a4
anders.poirel <contact@anderspoirel.net>
2025-06-22 19:17:23 -0700

    add explanatory variable

--- src/localbricks/spark.py
+++ src/localbricks/spark.py
@@ -5,7 +5,11 @@ from databricks.connect import DatabricksSession
 
 from .platform import is_databricks_driver
 
-
+# On a Databricks cluster driver in Standard access mode (a.k.a shared) and on 
+# Serveless Databrickscompute, the global 'spark' variable is a Spark Connect session.
+# In Dedicated access mode (a.k.a. single user), 'spark' is a regular Spark session.
+# Finally, a session created through Databricks Connect will always be a Spark Connect
+# session.
 def get_spark_session() -> SparkSession | SparkConnectSession:
     if is_databricks_driver():
         return SparkSession.builder.getOrCreate()
