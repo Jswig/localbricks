063c4fca7beea9c102e5a605686964a62e199922
anders.poirel <contact@anderspoirel.net>
2025-06-24 13:59:08 -0700

    use uv to run docs build

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -5,4 +5,4 @@ repo_root=$(git rev-parse --show-toplevel)
 
 uv sync --group docs
 echo "Building documentation..."
-sphinx-build -b html "${repo_root}/docs" "${repo_root}/docs/_build/html"
+uv run sphinx-build -b html "${repo_root}/docs" "${repo_root}/docs/_build/html"
