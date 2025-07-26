d33e23661ab770cb8662a95b981cb67744e704a9
Anders Poirel <contact@anderspoirel.net>
2025-07-26 16:27:55 -0700

    simplify package publishing

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,5 +4,12 @@ set -ex
 echo "Building package..."
 uv build
 
-echo "Publishing to PyPI..."
-uv publish
+if [ "$PUBLISH_TO_TESTPYPI" = "true" ]; then
+  export UV_PUBLISH_TOKEN="$TEST_PYPI_API_TOKEN"
+  echo "Publishing to TestPyPI..."
+  uv publish --index "https://test.pypi.org"
+else
+  export UV_PUBLISH_TOKEN="$PYPI_API_TOKEN"
+  echo "Publishing to PyPI..."
+  uv publish --index "https://upload.pypi.org/legacy/"
+fi
